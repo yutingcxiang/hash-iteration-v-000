b64cdf5921c867e3e1b1def8868db7ae0c1d0a77
Christine Xiang <yutingcxiang@gmail.com>
2018-07-09 06:28:58 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,10 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passenger.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
 end
